012f983c73dbf8659fbba9d6c7ee3cce67c2f429
PaulKim330 <kms30423@gmail.com>
2022-09-30 00:05:18 +0900

    책삭제하기 테스트 완료

--- src/test/java/com/example/junitproject/domain/BookRepositoryTest.java
+++ src/test/java/com/example/junitproject/domain/BookRepositoryTest.java
@@ -84,7 +84,28 @@ public class BookRepositoryTest {
     }
 
     //4. 책 수정 Update
+    @Test
+    public void 책수정_test(){
+        //given
+        Long id = 1L;
+        String title = "junit5";
+        String author = "메타코딩";
+        Book book = new Book(id, title, author);
+        //when
+        Book bookPS = bookRepository.save(book);
 
+//        bookRepository.findAll().stream()
+//                .forEach((b) -> {
+//                    System.out.println(b.getId());
+//                    System.out.println(b.getTitle());
+//                    System.out.println(b.getAuthor());
+//                    System.out.println("================================================================");
+//                });
+        //then
+        assertEquals(id, bookPS.getId());
+        assertEquals(title, bookPS.getTitle());
+        assertEquals(author, bookPS.getAuthor());
+    }
 
     //5. 책 삭제 Delete
     @Test
