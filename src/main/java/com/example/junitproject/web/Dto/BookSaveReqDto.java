bc2ec0d911f3abca2f43580c05d90cee93c10aa9
PaulKim330 <kms30423@gmail.com>
2022-09-29 17:26:35 +0900

    책한권조회, 책삭제 test 진행중 에러발생

--- src/main/java/com/example/junitproject/web/Dto/BookSaveReqDto.java
+++ src/main/java/com/example/junitproject/web/Dto/BookSaveReqDto.java
@@ -3,7 +3,7 @@ package com.example.junitproject.web.Dto;
 import com.example.junitproject.domain.Book;
 import lombok.Setter;
 
-@Setter
+@Setter //Controller 에서 Setter가 호출되면서 Dto에 값이 채워짐.
 public class BookSaveReqDto {
     private String title;
     private String author;
--- src/test/java/com/example/junitproject/domain/BookRepositoryTest.java
+++ src/test/java/com/example/junitproject/domain/BookRepositoryTest.java
@@ -1,12 +1,17 @@
 package com.example.junitproject.domain;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.jdbc.Sql;
 
 import java.util.List;
+import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest //DB와 관련된 컴포넌트들만 메모리에 로딩
 public class BookRepositoryTest {
@@ -14,9 +19,22 @@ public class BookRepositoryTest {
     @Autowired // DI
     private BookRepository bookRepository;
 
+    //@BeforeAll //테스트 시작전에 한번만 실행
+    @BeforeEach // 테스트 시작전에 한번씩 실행
+    public void 데이터준비(){
+        String title = "junit";
+        String author = "겟인데어";
+        Book book = Book.builder()
+                .title(title)
+                .author(author)
+                .build();
+        bookRepository.save(book);
+    }
+
 
     //1. 책 등록 Create
     @Test
+    @Order(1)
     public void 책등록_test(){
         //given(데이터 준비)
         String title = "junit5";
@@ -25,29 +43,60 @@ public class BookRepositoryTest {
                 .title(title)
                 .author(author)
                 .build();
-
+        //stub
         //when(테스트 실행)
         Book bookPS = bookRepository.save(book);
 
-        //then(검증)
+        //then(검증:AssertEquals(기댓값, 실제값))
+        assertEquals(title, bookPS.getTitle());
+        assertEquals(author, bookPS.getAuthor());
+    }//트랜잭션 종료(저장된 데이터를 초기화함)
+    //2. 책 목록보기 Read
+    @Test
+    public void 책목록보기_test(){
+        //given
+        String title = "junit";
+        String author = "겟인데어";
+
+        //when
+        List<Book> bookPS = bookRepository.findAll();
+
+        //then
+        assertEquals(title, bookPS.get(0).getTitle());
+        assertEquals(author, bookPS.get(0).getAuthor());
+    }//트랜잭션 종료(저장된 데이터를 초기화함)
+
+    //3. 책 한건보기 Read-1
+    @Test
+    @Sql("classpath:db/tableInit.sql")
+    public void 책한권보기_test(){
+        //given
+        String title = "junit";
+        String author = "겟인데어";
+
+
+        //when
+        Book bookPS = bookRepository.findById(1L).get();
+        // then
         assertEquals(title, bookPS.getTitle());
         assertEquals(author, bookPS.getAuthor());
 
     }
-    @Test
-    //2. 책 목록보기 Read
-    public void 책목록보기_test(){
-        //given
-
-        //when
-        List<Book> books = bookRepository.findAll();
-
-        //then
-        System.out.println(books.size());
-    }
-    //3. 책 한건보기 Read-1
 
     //4. 책 수정 Update
 
+
     //5. 책 삭제 Delete
+    @Test
+    @Sql("classpath:db/tableInit.sql")
+    public void 책삭제_test() {
+        //given
+        Long id = 1L;
+
+        //when
+        bookRepository.deleteById(id);
+
+        //then
+        assertFalse(bookRepository.findById(id).isPresent());
+    }
 }
